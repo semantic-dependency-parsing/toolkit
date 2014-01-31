c83c2bd05219d03cd1e76e0c573f608dad4c6f0d
Marco Kuhlmann <marco.kuhlmann@liu.se>
2014-01-31 14:55:16 +0100

    Add a tool to split the training data into train/dev data according to the recommendations in our README.

---
+++ src/sdp/tools/Splitter.java
@@ -0,0 +1,32 @@
+/*
+ * See the file "LICENSE" for the full license governing this code.
+ */
+package sdp.tools;
+
+import sdp.graph.Graph;
+import sdp.io.GraphReader;
+import sdp.io.GraphWriter;
+
+/**
+ *
+ * @author Marco Kuhlmann <marco.kuhlmann@liu.se>
+ */
+public class Splitter {
+
+    public static void main(String[] args) throws Exception {
+        GraphReader reader = new GraphReader(args[0]);
+        GraphWriter writerTrain = new GraphWriter(args[1]);
+        GraphWriter writerDevel = new GraphWriter(args[2]);
+        Graph graph;
+        while ((graph = reader.readGraph()) != null) {
+            if (graph.id.substring(2, 4).equals("20")) {
+                writerDevel.writeGraph(graph);
+            } else {
+                writerTrain.writeGraph(graph);
+            }
+        }
+        reader.close();
+        writerTrain.close();
+        writerDevel.close();
+    }
+}
