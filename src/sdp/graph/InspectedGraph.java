5b90ef8e3dbb295f6e40e6d034b2fbeaa3f1d446
Marco Kuhlmann <marco.kuhlmann@liu.se>
2014-05-21 23:00:29 +0200

    Clean up the Analyzer

--- src/sdp/graph/InspectedGraph.java
+++ src/sdp/graph/InspectedGraph.java
@@ -10,307 +10,221 @@ package sdp.graph;
  */
 public class InspectedGraph {
 
-    /**
-     * The analyzed graph.
-     */
-    private final Graph graph;
-    /**
-     * The index of the run during which this node was (first) visited.
-     */
-    private final int[] run;
-    /**
-     * The number of runs.
-     */
-    private int nRuns;
-    /**
-     * The preorder timestamps of each node.
-     */
-    private final int[] enter;
-    /**
-     * The postorder timestamps of each node.
-     */
-    private final int[] leave;
-    /**
-     * Flags indicating whether a node is a singleton.
-     */
-    private final boolean[] isSingleton;
-    /**
-     * The number of singleton nodes in this graph.
-     */
-    private final int nSingletons;
-
-    /**
-     * Construct a new inspector for the specified graph.
-     *
-     * @param graph the graph to be analyzed
-     */
-    public InspectedGraph(Graph graph) {
-	this.graph = graph;
-
-	int nNodes = graph.getNNodes();
-
-	this.run = new int[nNodes];
-
-	this.enter = new int[nNodes];
-	this.leave = new int[nNodes];
-	computeTimestamps();
-
-	this.isSingleton = new boolean[nNodes];
-	this.nSingletons = computeSingletons();
-    }
-
-    /**
-     * Computes the preorder and postorder timestamps for the inspected graph.
-     */
-    private void computeTimestamps() {
-	Timer timer = new Timer();
-	for (Node node : graph.getNodes()) {
-	    if (enter[node.id] == 0) {
-		computeTimestamps(node, timer);
-		nRuns++;
-	    }
-	}
-    }
-
-    /**
-     * Computes the preorder and postorder timestamps for the subgraph starting
-     * at the specified node.
-     *
-     * @param node the entry point for the subgraph
-     * @param timer the global timer
-     */
-    private void computeTimestamps(Node node, Timer timer) {
-	run[node.id] = nRuns;
-	enter[node.id] = timer.tick();
-	for (Edge outgoingEdge : node.getOutgoingEdges()) {
-	    // Only visit nodes that have not been visited before.
-	    if (enter[outgoingEdge.target] == 0) {
-		computeTimestamps(graph.getNode(outgoingEdge.target), timer);
-	    }
-	}
-	leave[node.id] = timer.tick();
-    }
-
-    /**
-     * Timer used in depth-first search.
-     */
-    private static final class Timer {
-
 	/**
-	 * The current time.
+	 * The analyzed graph.
 	 */
-	private int time;
+	private final Graph graph;
 
 	/**
-	 * Returns the current time, then increments it.
+	 * The number of non-wall node of this graph.
+	 */
+	private final int nNonWallNodes;
+
+	/**
+	 * DFS of the graph.
+	 */
+	private final DFS directedDFS;
+
+	/**
+	 * DFS of the undirected graph.
+	 */
+	private final DFS undirectedDFS;
+
+	/**
+	 * Flags indicating whether a node is a singleton.
+	 */
+	private final boolean[] isSingleton;
+
+	/**
+	 * The number of singleton nodes in this graph.
+	 */
+	private final int nSingletons;
+
+	/**
+	 * Construct a new inspector for the specified graph.
 	 *
-	 * @return the current time
+	 * @param graph the graph to be inspected
 	 */
-	public int tick() {
-	    return time++;
+	public InspectedGraph(Graph graph) {
+		this.graph = graph;
+
+		int nNodes = graph.getNNodes();
+		this.nNonWallNodes = nNodes - 1;
+		this.isSingleton = new boolean[nNodes];
+		this.nSingletons = computeSingletons();
+
+		this.directedDFS = new DFS(graph);
+		this.undirectedDFS = new DFS(graph, true);
 	}
-    }
 
-    /**
-     * Tests whether the inspected graph contains a cycle.
-     *
-     * @return {@code true} if and only if the inspected graph contains a cycle
-     */
-    public boolean isCyclic() {
-	for (Edge edge : graph.getEdges()) {
-	    // Check whether the current edge is a self-loop or a back edge.
-	    if (edge.target == edge.source || enter[edge.target] < enter[edge.source] && leave[edge.source] < leave[edge.target]) {
-		return true;
-	    }
+	/**
+	 * Returns the number of non-wall nodes of the inspected graph.
+	 *
+	 * @return the number of non-wall nodes of the inspected graph
+	 */
+	public int getNNonWallNodes() {
+		return nNonWallNodes;
 	}
-	return false;
-    }
 
-    /**
-     * Computes flags indicating whether a node is a singleton.
-     */
-    private int computeSingletons() {
-	int n = 0;
-	for (Node node : graph.getNodes()) {
-	    if (node.id != 0 && !node.hasIncomingEdges() && !node.hasOutgoingEdges() && !node.isTop) {
-		isSingleton[node.id] = true;
-		n++;
-	    }
+	/**
+	 * Returns the number of weakly connected components of the inspected graph.
+	 *
+	 * @return The number of weakly connected components of the inspected graph
+	 */
+	public int getNComponents() {
+		return undirectedDFS.getNRuns();
 	}
-	return n;
-    }
 
-    /**
-     * Tests whether the specified node is a singleton.
-     *
-     * @param id a node id
-     * @return {@code true} if the specified node is a singleton
-     */
-    public boolean isSingleton(int id) {
-	return isSingleton[id];
-    }
-
-    /**
-     * Returns the number of singleton nodes of this graph. A node is a
-     * singleton if it has no neighbors and is not a top node.
-     */
-    public int getNSingletons() {
-	return nSingletons;
-    }
-
-    /**
-     * Computes the maximal indegree of the non-trivial nodes in the inspected
-     * graph.
-     *
-     * @return the maximal indegree of the non-trivial nodes in the inspected
-     * graph
-     */
-    public int getMaximalIndegree() {
-	int max = 0;
-	for (Node node : graph.getNodes()) {
-	    if (!isSingleton[node.id]) {
-		max = Math.max(max, node.getNIncomingEdges());
-	    }
+	/**
+	 * Tests whether the inspected graph contains a cycle.
+	 *
+	 * @return {@code true} if and only if the inspected graph contains a cycle
+	 */
+	public boolean isCyclic() {
+		return directedDFS.isCyclic();
 	}
-	return max;
-    }
 
-    /**
-     * Computes the maximal outdegree of the non-trivial nodes in the inspected
-     * graph.
-     *
-     * @return the maximal outdegree of the non-trivial nodes in the inspected
-     * graph
-     */
-    public int getMaximalOutdegree() {
-	int max = 0;
-	for (Node node : graph.getNodes()) {
-	    if (!isSingleton[node.id]) {
-		max = Math.max(max, node.getNOutgoingEdges());
-	    }
-	}
-	return max;
-    }
-
-    /**
-     * Returns the number of non-trivial root nodes in the inspected graph. A
-     * <em>root node</em> is a node without incoming edges.
-     *
-     * @return the number of non-trivial root nodes in the inspected graph
-     */
-    public int getNRootNodes() {
-	int nRootNodes = 0;
-	for (Node node : graph.getNodes()) {
-	    if (node.id != 0 && !isSingleton[node.id]) {
-		nRootNodes += node.hasIncomingEdges() ? 0 : 1;
-	    }
-	}
-	return nRootNodes;
-    }
-
-    /**
-     * Returns the number of non-trivial leaf nodes in the inspected graph. A
-     * <em>leaf node</em> is a node without outgoing edges.
-     *
-     * @return the number of non-trivial leaf nodes in the inspected graph
-     */
-    public int getNLeafNodes() {
-	int nLeafNodes = 0;
-	for (Node node : graph.getNodes()) {
-	    if (node.id != 0 && !isSingleton[node.id]) {
-		nLeafNodes += node.hasOutgoingEdges() ? 0 : 1;
-	    }
-	}
-	return nLeafNodes;
-    }
-
-    /**
-     * Tests whether the inspected graph is a forest. A forest is an acyclic
-     * graph in which every node has at most one incoming edge.
-     *
-     * @return {@code true} if and only if the inspected graph is a forest
-     */
-    public boolean isForest() {
-	return !isCyclic() && getMaximalIndegree() <= 1;
-    }
-
-    /**
-     * Tests whether the inspected graph is a tree. A tree is a forest with
-     * exactly one root node.
-     *
-     * @return {@code true} if and only if the inspected graph is a tree
-     */
-    public boolean isTree() {
-	return isForest() && getNRootNodes() == 1;
-    }
-
-    /**
-     * Tests whether the inspected graph is projective. A graph is projective if
-     * there are no overlapping edges, and no edge covers some root node.
-     *
-     * @return {@code true} if and only if the inspected graph is projective
-     */
-    public boolean isProjective() {
-	int nNodes = graph.getNNodes();
-	boolean[] hasIncomingEdge = new boolean[nNodes];
-	boolean[] isCovered = new boolean[nNodes];
-	for (Edge edge1 : graph.getEdges()) {
-	    int min1 = Math.min(edge1.source, edge1.target);
-	    int max1 = Math.max(edge1.source, edge1.target);
-	    for (Edge edge2 : graph.getEdges()) {
-		int min2 = Math.min(edge2.source, edge2.target);
-		int max2 = Math.max(edge2.source, edge2.target);
-		if (overlap(min1, max1, min2, max2)) {
-		    return false;
+	/**
+	 * Computes flags indicating whether a node is a singleton.
+	 */
+	private int computeSingletons() {
+		int n = 0;
+		for (Node node : graph.getNodes()) {
+			if (node.id != 0 && !node.hasIncomingEdges() && !node.hasOutgoingEdges() && !node.isTop) {
+				isSingleton[node.id] = true;
+				n++;
+			}
 		}
-	    }
-	    hasIncomingEdge[edge1.target] = true;
-	    for (int i = min1 + 1; i < max1; i++) {
-		isCovered[i] = true;
-	    }
+		return n;
 	}
-	for (int node = 0; node < nNodes; node++) {
-	    if (!hasIncomingEdge[node] && isCovered[node] && !isSingleton[node]) {
-		return false;
-	    }
-	}
-	return true;
-    }
 
-    /**
-     * Tests whether the specified edges overlap (cross).
-     *
-     * @param min1 the position of the left node of the first edge
-     * @param max1 the position of the right node of the first edge
-     * @param min2 the position of the left node of the second edge
-     * @param max2 the position of the right node of the second edge
-     * @return {@code true} if and only if the specified edges overlap
-     */
-    private static boolean overlap(int min1, int max1, int min2, int max2) {
-	return min1 < min2 && min2 < max1 && max1 < max2 || min2 < min1 && min1 < max2 && max2 < max1;
-    }
+	/**
+	 * Tests whether the specified node is a singleton.
+	 *
+	 * @param id a node id
+	 * @return {@code true} if the specified node is a singleton
+	 */
+	public boolean isSingleton(int id) {
+		return isSingleton[id];
+	}
 
-    /**
-     * Returns the number of weakly connected components of the inspected graph.
-     *
-     * @return The number of weakly connected components of the inspected graph
-     */
-    public int getNComponents() {
-	int[] component = new int[nRuns];
-	for (int i = 0; i < nRuns; i++) {
-	    component[i] = i;
+	/**
+	 * Returns the number of singleton nodes of this graph. A node is a
+	 * singleton if it has no neighbors and is not a top node.
+	 *
+	 * @return the number of singleton nodes of this graph
+	 */
+	public int getNSingletons() {
+		return nSingletons;
 	}
-	int nComponents = nRuns;
-	for (Node node : graph.getNodes()) {
-	    nComponents -= isSingleton[node.id] ? 1 : 0;
+
+	/**
+	 * Computes the maximal indegree of the nodes in the inspected graph.
+	 *
+	 * @return the maximal indegree of the nodes in the inspected graph
+	 */
+	public int getMaximalIndegree() {
+		int max = 0;
+		for (Node node : graph.getNodes()) {
+			max = Math.max(max, node.getNIncomingEdges());
+		}
+		return max;
 	}
-	for (Edge edge : graph.getEdges()) {
-	    if (component[run[edge.source]] != component[run[edge.target]]) {
-		component[run[edge.source]] = component[run[edge.target]];
-		nComponents--;
-	    }
+
+	/**
+	 * Computes the maximal outdegree of the nodes in the inspected graph.
+	 *
+	 * @return the maximal outdegree of the nodes in the inspected graph
+	 */
+	public int getMaximalOutdegree() {
+		int max = 0;
+		for (Node node : graph.getNodes()) {
+			max = Math.max(max, node.getNOutgoingEdges());
+		}
+		return max;
 	}
-	assert nComponents >= 0;
-	return nComponents;
-    }
+
+	/**
+	 * Returns the number of root nodes in the inspected graph. A
+	 * <em>root node</em> is a node without incoming edges. The wall node is not
+	 * considered to be a root node.
+	 *
+	 * @return the number of root nodes in the inspected graph
+	 */
+	public int getNRootNodes() {
+		int nRootNodes = 0;
+		for (Node node : graph.getNodes()) {
+			nRootNodes += node.hasIncomingEdges() ? 0 : 1;
+		}
+		return nRootNodes - 1; // the wall node
+	}
+
+	/**
+	 * Returns the number of leaf nodes in the inspected graph. A
+	 * <em>leaf node</em> is a node without outgoing edges. The wall node is not
+	 * considered to be a leaf node.
+	 *
+	 * @return the number of leaf nodes in the inspected graph
+	 */
+	public int getNLeafNodes() {
+		int nLeafNodes = 0;
+		for (Node node : graph.getNodes()) {
+			nLeafNodes += node.hasOutgoingEdges() ? 0 : 1;
+		}
+		return nLeafNodes - 1;//the wall node
+	}
+
+	/**
+	 * Tests whether the inspected graph is a forest. A forest is an acyclic
+	 * graph in which every node has at most one incoming edge.
+	 *
+	 * @return {@code true} if and only if the inspected graph is a forest
+	 */
+	public boolean isForest() {
+		return !isCyclic() && getMaximalIndegree() <= 1;
+	}
+
+	/**
+	 * Tests whether the inspected graph is a tree. A tree is a forest with
+	 * exactly one root node.
+	 *
+	 * @return {@code true} if and only if the inspected graph is a tree
+	 */
+	public boolean isTree() {
+		return isForest() && getNRootNodes() == 1;
+	}
+
+	/**
+	 * Tests whether the inspected graph is projective. A graph is projective if
+	 * there are no overlapping edges.
+	 *
+	 * @return {@code true} if and only if the inspected graph is projective
+	 */
+	public boolean isProjective() {
+		for (Edge edge1 : graph.getEdges()) {
+			int min1 = Math.min(edge1.source, edge1.target);
+			int max1 = Math.max(edge1.source, edge1.target);
+			for (Edge edge2 : graph.getEdges()) {
+				int min2 = Math.min(edge2.source, edge2.target);
+				int max2 = Math.max(edge2.source, edge2.target);
+				if (overlap(min1, max1, min2, max2)) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+
+	/**
+	 * Tests whether the specified edges overlap (cross).
+	 *
+	 * @param min1 the position of the left node of the first edge
+	 * @param max1 the position of the right node of the first edge
+	 * @param min2 the position of the left node of the second edge
+	 * @param max2 the position of the right node of the second edge
+	 * @return {@code true} if and only if the specified edges overlap
+	 */
+	private static boolean overlap(int min1, int max1, int min2, int max2) {
+		return min1 < min2 && min2 < max1 && max1 < max2 || min2 < min1 && min1 < max2 && max2 < max1;
+	}
 }
--- src/sdp/tools/Analyzer.java
+++ src/sdp/tools/Analyzer.java
@@ -3,7 +3,9 @@
  */
 package sdp.tools;
 
+import java.text.NumberFormat;
 import java.util.HashSet;
+import java.util.Locale;
 import java.util.Set;
 import sdp.graph.Edge;
 import sdp.graph.Graph;
@@ -18,197 +20,151 @@ import sdp.io.GraphReader;
  */
 public class Analyzer {
 
-    // The number of graphs read.
-    private int nGraphs;
+	// The number of graphs read.
+	private int nGraphs;
 
-    // Number of labels seen.
-    private final Set<String> labels = new HashSet<String>();
+	// The number of non-wall nodes seen.
+	private int nNonWallNodes;
 
-    // The number of nodes seen (excluding wall nodes).
-    private int nNodes;
+	// The number of edges seen.
+	private int nEdges;
 
-    // Counter to compute the average number of top nodes per graph.
-    private double avgNTopNodes;
+	// Number of labels seen.
+	private final Set<String> labels = new HashSet<String>();
 
-    // Counter to compute the average number of structutral roots per graph.
-    private double avgNStructuralRoots;
+	// The number of singleton nodes.
+	private int nSingletons;
 
-    // Counter to compute the percentage of semi-connected graphs.
-    private double pcSemiConnected;
+	// Number of cyclic graphs.
+	private int nCyclic;
 
-    // Counter to compute the percentage of non-singleton nodes that are reentrant.
-    private double pcReentrant;
+	// Number of forests.
+	private int nForests;
 
-    // Counter to compute the number of cyclic graphs.
-    private double pcCyclic;
+	// Number of trees.
+	private int nTrees;
 
-    // Counter to compute the maximal indegree of a node.
-    private int maxIndegreeGlobal;
+	// Number of graphs that are semi-connected.
+	private int nSemiConnected;
 
-    // Counter to compute the average indegree per node.
-    private double avgIndegreeGlobal;
+	// Number of nodes that have more than one incoming edge.
+	private int nReentrantNodes;
 
-    // Counter to compute the maximal outdegree of a node.
-    private int maxOutdegreeGlobal;
+	// Number of topless graphs.
+	private int nToplessGraphs;
 
-    // Counter to compute the average outdegree per node.
-    private double avgOutdegreeGlobal;
+	// Number of top nodes.
+	private int nTopNodes;
 
-    // Counter to compute the average number of singletons per graph.
-    private double avgSingletons;
+	// Number of special nodes.
+	private int nSpecialNodes;
 
-    // Counter to compute the percentage of singleton nodes.
-    private double pcSingletons;
+	/**
+	 * Updates the statistics with the specified graph.
+	 *
+	 * @param graph a semantic dependency graph
+	 */
+	public void update(Graph graph) {
+		InspectedGraph inspectedGraph = new InspectedGraph(graph);
 
-    // Counter to compute the percentage of topless graphs.
-    private double pcToplessGraphs;
+		// number of graphs
+		nGraphs++;
 
-    // Counter to compute the percentage of graphs that are forests.
-    private double pcForests;
+		// number of non-wall nodes
+		nNonWallNodes += inspectedGraph.getNNonWallNodes();
 
-    // Counter to compute the percentage of graphs that are trees.
-    private double pcTrees;
+		// number of edges
+		nEdges += graph.getNEdges();
 
-    // Counter to compute the edge density.
-    private double edgeDensity;
+		// distinct labels
+		for (Edge edge : graph.getEdges()) {
+			labels.add(edge.label);
+		}
 
-    // Counter to compute the percentage of (non-singleton) nodes with in-degree = 0 that are not top.
-    private double pcSpecial;
+		// number of singleton nodes
+		nSingletons += inspectedGraph.getNSingletons();
 
-    /**
-     * Updates the statistics with the specified graph.
-     *
-     * @param graph a semantic dependency graph
-     */
-    public void update(Graph graph) {
-	InspectedGraph inspectedGraph = new InspectedGraph(graph);
+		// number of cyclic graphs
+		nCyclic += inspectedGraph.isCyclic() ? 1 : 0;
 
-	int nTopNodes = 0;
-	int nEdgesFromTopNode = 0;
-	int nStructuralRoots = 0;
-	boolean isReentrant = false;
-	boolean isCyclic = false;
-	boolean isSemiConnected = true;
-	int maxIndegree = 0;
-	double avgIndegree = 0.0;
-	int maxOutdegree = 0;
-	double avgOutdegree = 0.0;
-	int nSingletons = inspectedGraph.getNSingletons();
+		// number of forests
+		nForests += inspectedGraph.isForest() ? 1 : 0;
 
-	for (Node node : graph.getNodes()) {
-	    if (node.isTop) {
-		nTopNodes++;
-		nEdgesFromTopNode += node.getNOutgoingEdges();
-	    }
-	    if (!node.hasIncomingEdges()) {
-		nStructuralRoots++;
-	    }
-	    if (node.getNIncomingEdges() >= 2) {
-		isReentrant = true;
-	    }
-	    if (!inspectedGraph.isSingleton(node.id)) {
-		maxIndegree = Math.max(maxIndegree, node.getNIncomingEdges());
-		avgIndegree += node.getNIncomingEdges();
-		maxOutdegree = Math.max(maxOutdegree, node.getNOutgoingEdges());
-		avgOutdegree += node.getNOutgoingEdges();
-		avgIndegreeGlobal += node.getNIncomingEdges();
-		avgOutdegreeGlobal += node.getNOutgoingEdges();
-		pcSpecial += (!node.hasIncomingEdges() && !node.isTop) ? 1 : 0;
-	    }
-	}
-	if (inspectedGraph.isCyclic()) {
-	    isCyclic = true;
-	}
-	if (inspectedGraph.getNComponents() > 1) {
-	    isSemiConnected = false;
-	}
-	avgIndegree /= (double) graph.getNNodes();
-	avgOutdegree /= (double) graph.getNNodes();
+		// number of trees
+		nTrees += inspectedGraph.isTree() ? 1 : 0;
 
-	nGraphs++;
-	nNodes += graph.getNNodes() - 1;
+		// number of graphs that are semi-connected
+		nSemiConnected += inspectedGraph.getNComponents() - 1 - inspectedGraph.getNSingletons() == 1 ? 1 : 0;
 
-	avgNTopNodes += nTopNodes;
-	avgNStructuralRoots += nTopNodes;
-	pcSemiConnected += isSemiConnected ? 1.0 : 0.0;
-	pcReentrant += isReentrant ? 1.0 : 0.0;
-	pcCyclic += isCyclic ? 1.0 : 0.0;
-	maxIndegreeGlobal = Math.max(maxIndegreeGlobal, maxIndegree);
-	avgSingletons += nSingletons;
-	pcSingletons += nSingletons;
-	pcToplessGraphs += nTopNodes == 0 ? 1.0 : 0.0;
-	pcForests += inspectedGraph.isForest() ? 1.0 : 0.0;
-	pcTrees += inspectedGraph.isTree() ? 1.0 : 0.0;
-	edgeDensity += graph.getNEdges();
+		// number of reentrant nodes
+		for (Node node : graph.getNodes()) {
+			nReentrantNodes += node.getNIncomingEdges() > 1 ? 1 : 0;
+		}
 
-	for (Edge edge : graph.getEdges()) {
-	    labels.add(edge.label);
+		// number of topless graphs
+		boolean isTopless = true;
+		for (Node node : graph.getNodes()) {
+			isTopless = isTopless && !node.isTop;
+		}
+		nToplessGraphs += isTopless ? 1 : 0;
+
+		// number of top nodes
+		for (Node node : graph.getNodes()) {
+			nTopNodes += node.isTop ? 1 : 0;
+		}
+
+		// number of special nodes
+		for (Node node : graph.getNodes()) {
+			nSpecialNodes += !inspectedGraph.isSingleton(node.id) && !node.hasIncomingEdges() && !node.isTop ? 1 : 0;
+		}
 	}
 
-	// Print graph-specific statistics.
-	System.out.format("%s", graph.id);
-	// number of top nodes
-	System.out.format("\t%d", nTopNodes);
-	// number of outgoing arcs from top
-	System.out.format("\t%d", nEdgesFromTopNode);
-	// cyclic?
-	System.out.format("\t%s", inspectedGraph.isCyclic() ? "+" : "-");
-	// semiconnected?
-	System.out.format("\t%s", isSemiConnected ? "+" : "-");
-	System.out.println();
-    }
-
-    /**
-     * Finalizes the computation of the statistics.
-     */
-    public void finish() {
-	double nNonSingletonNodes = nNodes - pcSingletons;
-	//
-	pcSingletons /= nNodes;
-	//
-	avgIndegreeGlobal /= nNonSingletonNodes;
-	avgOutdegreeGlobal /= nNonSingletonNodes;
-	pcReentrant /= nNonSingletonNodes;
-	pcSpecial /= nNonSingletonNodes;
-	edgeDensity /= nNonSingletonNodes;
-	//
-	avgNTopNodes /= nGraphs;
-	avgNStructuralRoots /= nGraphs;
-	pcSemiConnected /= nGraphs;
-	pcCyclic /= nGraphs;
-	avgSingletons /= nGraphs;
-	pcToplessGraphs /= nGraphs;
-	pcForests /= nGraphs;
-	pcTrees /= nGraphs;
-    }
-
-    /**
-     * Prints statistics about a set of graphs.
-     *
-     * @param args names of files from which to read graphs
-     * @throws Exception if an I/O exception occurs
-     */
-    public static void main(String[] args) throws Exception {
-	Analyzer analyzer = new Analyzer();
-	for (String arg : args) {
-	    GraphReader reader = new GraphReader(arg);
-	    Graph graph;
-	    while ((graph = reader.readGraph()) != null) {
-		analyzer.update(graph);
-	    }
-	    reader.close();
+	/**
+	 * Prints statistics about a set of graphs.
+	 *
+	 * @param args names of files from which to read graphs
+	 * @throws Exception if an I/O exception occurs
+	 */
+	public static void main(String[] args) throws Exception {
+		Analyzer analyzer = new Analyzer();
+		for (String arg : args) {
+			GraphReader reader = new GraphReader(arg);
+			Graph graph;
+			while ((graph = reader.readGraph()) != null) {
+				analyzer.update(graph);
+			}
+			reader.close();
+		}
+		System.err.format("number of graphs: %d%n", analyzer.nGraphs);
+		System.err.format("number of non-wall nodes: %d%n", analyzer.nNonWallNodes);
+		System.err.format("number of distinct labels: %d%n", analyzer.labels.size());
+		System.err.format("percentage of singleton nodes (over all non-wall nodes): %s%n", percentage(analyzer.nSingletons, analyzer.nNonWallNodes));
+		System.err.format("edge density (proportion of edge counts to non-singleton nodes): %s%n", fraction(analyzer.nEdges, analyzer.nNonWallNodes - analyzer.nSingletons));
+		System.err.format("percentage of cyclic graphs: %s%n", percentage(analyzer.nCyclic, analyzer.nGraphs));
+		System.err.format("percentage of graphs that are forests: %s%n", percentage(analyzer.nForests, analyzer.nGraphs));
+		System.err.format("percentage of graphs that are trees: %s%n", percentage(analyzer.nTrees, analyzer.nGraphs));
+		System.err.format("percentage of graphs that are semi-connected: %s%n", percentage(analyzer.nSemiConnected, analyzer.nGraphs));
+		System.err.format("percentage of (non-singleton) nodes that are reentrant: %s%n", percentage(analyzer.nReentrantNodes, analyzer.nNonWallNodes - analyzer.nSingletons));
+		System.err.format("percentage of topless graphs: %s%n", percentage(analyzer.nToplessGraphs, analyzer.nGraphs));
+		System.err.format("average number of top nodes per graph: %s%n", fraction(analyzer.nTopNodes, analyzer.nGraphs));
+		System.err.format("percentage of (non-singleton) nodes with indegree = 0 that are not top: %s%n", percentage(analyzer.nSpecialNodes, analyzer.nNonWallNodes - analyzer.nSingletons));
 	}
-	analyzer.finish();
-	System.err.format("number of graphs: %d%n", analyzer.nGraphs);
-	System.err.format("number of distinct labels: %d%n", analyzer.labels.size());
-	System.err.format("percentage of singleton nodes (over all nodes): %f%n", analyzer.pcSingletons);
-	System.err.format("edge density (proportion of edge counts to non-singleton nodes): %f%n", analyzer.edgeDensity);
-	System.err.format("percentage of cyclic graphs: %f%n", analyzer.pcCyclic);
-	System.err.format("percentage of graphs that are forests: %f%n", analyzer.pcForests);
-	System.err.format("percentage of graphs that are semi-connected: %f%n", analyzer.pcSemiConnected);
-	System.err.format("percentage of (non-singleton) nodes that are reentrant: %f%n", analyzer.pcReentrant);
-	System.err.format("percentage of topless graphs: %f%n", analyzer.pcToplessGraphs);
-	System.err.format("average number of top nodes per graph: %f%n", analyzer.avgNTopNodes);
-	System.err.format("percentage of (non-singleton) nodes with indegree = 0 that are not top: %f%n", analyzer.pcSpecial);
-    }
+
+	private static final NumberFormat NUMBER_FORMAT = NumberFormat.getIntegerInstance(Locale.ENGLISH);
+	private static final NumberFormat PERCENT_FORMAT = NumberFormat.getPercentInstance(Locale.ENGLISH);
+
+	static {
+		NUMBER_FORMAT.setMinimumFractionDigits(4);
+		NUMBER_FORMAT.setMaximumFractionDigits(4);
+		PERCENT_FORMAT.setMinimumFractionDigits(2);
+		PERCENT_FORMAT.setMaximumFractionDigits(2);
+	}
+
+	public static String fraction(int a, int b) {
+		return NUMBER_FORMAT.format((double) a / (double) b);
+	}
+
+	public static String percentage(int enumerator, int denominator) {
+		return PERCENT_FORMAT.format((double) enumerator / (double) denominator);
+	}
 }
