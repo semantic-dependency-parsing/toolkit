6127be48b8bfa7a1472996c20f97627af5e3328f
Marco Kuhlmann <marco.kuhlmann@liu.se>
2014-05-21 17:45:09 +0200

    Start to implement new graph analytics

--- src/sdp/tools/Analyzer.java
+++ src/sdp/tools/Analyzer.java
@@ -3,6 +3,9 @@
  */
 package sdp.tools;
 
+import java.util.HashSet;
+import java.util.Set;
+import sdp.graph.Edge;
 import sdp.graph.Graph;
 import sdp.graph.InspectedGraph;
 import sdp.graph.Node;
@@ -18,7 +21,10 @@ public class Analyzer {
     // The number of graphs read.
     private int nGraphs;
 
-    // The number of nodes seen.
+    // Number of labels seen.
+    private final Set<String> labels = new HashSet<String>();
+
+    // The number of nodes seen (excluding wall nodes).
     private int nNodes;
 
     // Counter to compute the average number of top nodes per graph.
@@ -30,7 +36,7 @@ public class Analyzer {
     // Counter to compute the percentage of semi-connected graphs.
     private double pcSemiConnected;
 
-    // Counter to compute the percentage of reentrant graphs.
+    // Counter to compute the percentage of non-singleton nodes that are reentrant.
     private double pcReentrant;
 
     // Counter to compute the number of cyclic graphs.
@@ -51,90 +57,129 @@ public class Analyzer {
     // Counter to compute the average number of singletons per graph.
     private double avgSingletons;
 
+    // Counter to compute the percentage of singleton nodes.
+    private double pcSingletons;
+
+    // Counter to compute the percentage of topless graphs.
+    private double pcToplessGraphs;
+
+    // Counter to compute the percentage of graphs that are forests.
+    private double pcForests;
+
+    // Counter to compute the percentage of graphs that are trees.
+    private double pcTrees;
+
+    // Counter to compute the edge density.
+    private double edgeDensity;
+
+    // Counter to compute the percentage of (non-singleton) nodes with in-degree = 0 that are not top.
+    private double pcSpecial;
+
     /**
      * Updates the statistics with the specified graph.
      *
      * @param graph a semantic dependency graph
      */
     public void update(Graph graph) {
-        InspectedGraph inspectedGraph = new InspectedGraph(graph);
+	InspectedGraph inspectedGraph = new InspectedGraph(graph);
 
-        int nTopNodes = 0;
-        int nEdgesFromTopNode = 0;
-        int nStructuralRoots = 0;
-        boolean isReentrant = false;
-        boolean isCyclic = false;
-        boolean isSemiConnected = true;
-        int maxIndegree = 0;
-        double avgIndegree = 0.0;
-        int maxOutdegree = 0;
-        double avgOutdegree = 0.0;
-		int nSingletons = inspectedGraph.getNSingletons();
+	int nTopNodes = 0;
+	int nEdgesFromTopNode = 0;
+	int nStructuralRoots = 0;
+	boolean isReentrant = false;
+	boolean isCyclic = false;
+	boolean isSemiConnected = true;
+	int maxIndegree = 0;
+	double avgIndegree = 0.0;
+	int maxOutdegree = 0;
+	double avgOutdegree = 0.0;
+	int nSingletons = inspectedGraph.getNSingletons();
 
-        for (Node node : graph.getNodes()) {
-            if (node.isTop) {
-                nTopNodes++;
-                nEdgesFromTopNode += node.getNOutgoingEdges();
-            }
-            if (!node.hasIncomingEdges()) {
-                nStructuralRoots++;
-            }
-            if (node.getNIncomingEdges() >= 2) {
-                isReentrant = true;
-            }
-            maxIndegree = Math.max(maxIndegree, node.getNIncomingEdges());
-            avgIndegree += node.getNIncomingEdges();
-            maxOutdegree = Math.max(maxOutdegree, node.getNOutgoingEdges());
-            avgOutdegree += node.getNOutgoingEdges();
-            avgIndegreeGlobal += node.getNIncomingEdges();
-            avgOutdegreeGlobal += node.getNOutgoingEdges();
-        }
-        if (inspectedGraph.isCyclic()) {
-            isCyclic = true;
-        }
-        if (inspectedGraph.getNComponents() > 1) {
-            isSemiConnected = false;
-        }
-        avgIndegree /= (double) graph.getNNodes();
-        avgOutdegree /= (double) graph.getNNodes();
+	for (Node node : graph.getNodes()) {
+	    if (node.isTop) {
+		nTopNodes++;
+		nEdgesFromTopNode += node.getNOutgoingEdges();
+	    }
+	    if (!node.hasIncomingEdges()) {
+		nStructuralRoots++;
+	    }
+	    if (node.getNIncomingEdges() >= 2) {
+		isReentrant = true;
+	    }
+	    if (!inspectedGraph.isSingleton(node.id)) {
+		maxIndegree = Math.max(maxIndegree, node.getNIncomingEdges());
+		avgIndegree += node.getNIncomingEdges();
+		maxOutdegree = Math.max(maxOutdegree, node.getNOutgoingEdges());
+		avgOutdegree += node.getNOutgoingEdges();
+		avgIndegreeGlobal += node.getNIncomingEdges();
+		avgOutdegreeGlobal += node.getNOutgoingEdges();
+		pcSpecial += (!node.hasIncomingEdges() && !node.isTop) ? 1 : 0;
+	    }
+	}
+	if (inspectedGraph.isCyclic()) {
+	    isCyclic = true;
+	}
+	if (inspectedGraph.getNComponents() > 1) {
+	    isSemiConnected = false;
+	}
+	avgIndegree /= (double) graph.getNNodes();
+	avgOutdegree /= (double) graph.getNNodes();
 
-        nGraphs++;
-        nNodes += graph.getNNodes();
+	nGraphs++;
+	nNodes += graph.getNNodes() - 1;
 
-        avgNTopNodes += nTopNodes;
-        avgNStructuralRoots += nTopNodes;
-        pcSemiConnected += isSemiConnected ? 1.0 : 0.0;
-        pcReentrant += isReentrant ? 1.0 : 0.0;
-        pcCyclic += isCyclic ? 1.0 : 0.0;
-        maxIndegreeGlobal = Math.max(maxIndegreeGlobal, maxIndegree);
-        avgSingletons += nSingletons;
+	avgNTopNodes += nTopNodes;
+	avgNStructuralRoots += nTopNodes;
+	pcSemiConnected += isSemiConnected ? 1.0 : 0.0;
+	pcReentrant += isReentrant ? 1.0 : 0.0;
+	pcCyclic += isCyclic ? 1.0 : 0.0;
+	maxIndegreeGlobal = Math.max(maxIndegreeGlobal, maxIndegree);
+	avgSingletons += nSingletons;
+	pcSingletons += nSingletons;
+	pcToplessGraphs += nTopNodes == 0 ? 1.0 : 0.0;
+	pcForests += inspectedGraph.isForest() ? 1.0 : 0.0;
+	pcTrees += inspectedGraph.isTree() ? 1.0 : 0.0;
+	edgeDensity += graph.getNEdges();
 
-        // Print graph-specific statistics.
-        System.out.format("%s", graph.id);
-        // number of top nodes
-        System.out.format("\t%d", nTopNodes);
-        // number of outgoing arcs from top
-        System.out.format("\t%d", nEdgesFromTopNode);
-        // cyclic?
-        System.out.format("\t%s", inspectedGraph.isCyclic() ? "+" : "-");
-        // semiconnected?
-        System.out.format("\t%s", isSemiConnected ? "+" : "-");
-        System.out.println();
+	for (Edge edge : graph.getEdges()) {
+	    labels.add(edge.label);
+	}
+
+	// Print graph-specific statistics.
+	System.out.format("%s", graph.id);
+	// number of top nodes
+	System.out.format("\t%d", nTopNodes);
+	// number of outgoing arcs from top
+	System.out.format("\t%d", nEdgesFromTopNode);
+	// cyclic?
+	System.out.format("\t%s", inspectedGraph.isCyclic() ? "+" : "-");
+	// semiconnected?
+	System.out.format("\t%s", isSemiConnected ? "+" : "-");
+	System.out.println();
     }
 
     /**
      * Finalizes the computation of the statistics.
      */
     public void finish() {
-        avgIndegreeGlobal /= nNodes;
-        avgOutdegreeGlobal /= nNodes;
-        //
-        avgNTopNodes /= nGraphs;
-        avgNStructuralRoots /= nGraphs;
-        pcSemiConnected /= nGraphs;
-        pcReentrant /= nGraphs;
-        pcCyclic /= nGraphs;
-        avgSingletons /= nGraphs;
+	double nNonSingletonNodes = nNodes - pcSingletons;
+	//
+	pcSingletons /= nNodes;
+	//
+	avgIndegreeGlobal /= nNonSingletonNodes;
+	avgOutdegreeGlobal /= nNonSingletonNodes;
+	pcReentrant /= nNonSingletonNodes;
+	pcSpecial /= nNonSingletonNodes;
+	edgeDensity /= nNonSingletonNodes;
+	//
+	avgNTopNodes /= nGraphs;
+	avgNStructuralRoots /= nGraphs;
+	pcSemiConnected /= nGraphs;
+	pcCyclic /= nGraphs;
+	avgSingletons /= nGraphs;
+	pcToplessGraphs /= nGraphs;
+	pcForests /= nGraphs;
+	pcTrees /= nGraphs;
     }
 
     /**
@@ -144,19 +189,26 @@ public class Analyzer {
      * @throws Exception if an I/O exception occurs
      */
     public static void main(String[] args) throws Exception {
-        Analyzer analyzer = new Analyzer();
-        for (String arg : args) {
-            GraphReader reader = new GraphReader(arg);
-            Graph graph;
-            while ((graph = reader.readGraph()) != null) {
-                analyzer.update(graph);
-            }
-            reader.close();
-        }
-        analyzer.finish();
-        System.err.format("number of graphs: %d%n", analyzer.nGraphs);
-        System.err.format("average number of top nodes per graph: %f%n", analyzer.avgNTopNodes);
-        System.err.format("percentage of cyclic graphs: %f%n", analyzer.pcCyclic);
-        System.err.format("percentage of semi-connected graphs: %f%n", analyzer.pcSemiConnected);
+	Analyzer analyzer = new Analyzer();
+	for (String arg : args) {
+	    GraphReader reader = new GraphReader(arg);
+	    Graph graph;
+	    while ((graph = reader.readGraph()) != null) {
+		analyzer.update(graph);
+	    }
+	    reader.close();
+	}
+	analyzer.finish();
+	System.err.format("number of graphs: %d%n", analyzer.nGraphs);
+	System.err.format("number of distinct labels: %d%n", analyzer.labels.size());
+	System.err.format("percentage of singleton nodes (over all nodes): %f%n", analyzer.pcSingletons);
+	System.err.format("edge density (proportion of edge counts to non-singleton nodes): %f%n", analyzer.edgeDensity);
+	System.err.format("percentage of cyclic graphs: %f%n", analyzer.pcCyclic);
+	System.err.format("percentage of graphs that are forests: %f%n", analyzer.pcForests);
+	System.err.format("percentage of graphs that are semi-connected: %f%n", analyzer.pcSemiConnected);
+	System.err.format("percentage of (non-singleton) nodes that are reentrant: %f%n", analyzer.pcReentrant);
+	System.err.format("percentage of topless graphs: %f%n", analyzer.pcToplessGraphs);
+	System.err.format("average number of top nodes per graph: %f%n", analyzer.avgNTopNodes);
+	System.err.format("percentage of (non-singleton) nodes with indegree = 0 that are not top: %f%n", analyzer.pcSpecial);
     }
 }
