e7b4ef6f67d19c71024760a6d770640649af6220
Marco Kuhlmann <marco.kuhlmann@liu.se>
2014-09-11 16:46:28 +0200

    Create GraphReader, GraphWriter for the 2015 format

---
+++ src/sdp/io/GraphReader2015.java
@@ -0,0 +1,133 @@
+/*
+ * See the file "LICENSE" for the full license governing this code.
+ */
+package sdp.io;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.ArrayList;
+import java.util.List;
+import sdp.graph.Graph;
+import sdp.graph.Node;
+
+/**
+ * Read semantic dependency graphs from a file. This reader implements the
+ * format described at
+ * {@link http://alt.qcri.org/semeval2014/task8/index.php?id=dependency-formats}.
+ *
+ * @author Marco Kuhlmann <marco.kuhlmann@liu.se>
+ */
+public class GraphReader2015 extends ParagraphReader {
+
+	/**
+	 * Create a graph reader, using the default input-buffer size.
+	 *
+	 * @param reader a Reader object to provide the underlying stream
+	 */
+	public GraphReader2015(Reader reader) {
+		super(reader);
+	}
+
+	/**
+	 * Create a graph reader that reads from the specified file. The file will
+	 * be read using the default input-buffer size.
+	 *
+	 * @param file the file to read from
+	 * @throws FileNotFoundException if the specified file does not exist, is a
+	 * directory rather than a regular file, or for some other reason cannot be
+	 * opened for reading
+	 */
+	public GraphReader2015(File file) throws FileNotFoundException {
+		super(file);
+	}
+
+	/**
+	 * Create a graph reader that reads from the specified file. The file will
+	 * be read using the default input-buffer size.
+	 *
+	 * @param fileName the name of the file to read from
+	 * @throws FileNotFoundException if the specified file does not exist, is a
+	 * directory rather than a regular file, or for some other reason cannot be
+	 * opened for reading
+	 */
+	public GraphReader2015(String fileName) throws FileNotFoundException {
+		super(fileName);
+	}
+
+	/**
+	 * Reads a single graph.
+	 *
+	 * @return the graph read, or {@code null} if the end of the stream has been
+	 * reached
+	 * @throws IOException if an I/O error occurs
+	 */
+	public Graph readGraph() throws IOException {
+		List<String> lines = super.readParagraph();
+		if (lines == null) {
+			return null;
+		} else {
+			// Every graph should contain at least one token.
+			assert lines.size() >= 2;
+			// Assert the format of the graph ID.
+			assert lines.get(0).matches("#2[0-9]{7}$");
+
+			Graph graph = new Graph(lines.get(0));
+
+			// Add the wall node.
+			graph.addNode(Constants.WALL_FORM, Constants.WALL_LEMMA, Constants.WALL_POS, false, false, Constants.WALL_SENSE);
+
+			// Add the token nodes to the graph and collect a list of predicates.
+			List<Integer> predicates = new ArrayList<Integer>();
+			for (String line : lines.subList(1, lines.size())) {
+				String[] tokens = line.split(Constants.COLUMN_SEPARATOR);
+
+				// There should be at least seven columns: ID, FORM, LEMMA, POS, TOP, PRED, SENSE
+				assert tokens.length >= 7;
+				// Enforce valid values for the TOP column.
+				assert tokens[4].equals("+") || tokens[4].equals("-");
+				// Enforce valid values for the PRED column.
+				assert tokens[5].equals("+") || tokens[5].equals("-");
+
+				String form = tokens[1];
+				String lemma = tokens[2];
+				String pos = tokens[3];
+				boolean isTop = tokens[4].equals("+");
+				boolean isPred = tokens[5].equals("+");
+				String sense = tokens[6];
+
+				Node node = graph.addNode(form, lemma, pos, isTop, isPred, sense);
+				// Make sure that the node ID equals the value of the ID column.
+				assert node.id == Integer.parseInt(tokens[0]);
+
+				if (node.isPred) {
+					predicates.add(node.id);
+				}
+			}
+
+			// Add the edges to the graph.
+			int id = 1;
+			for (String line : lines.subList(1, lines.size())) {
+				String[] tokens = line.split(Constants.COLUMN_SEPARATOR);
+
+				// There should be exactly 7 + number of predicates many columns.
+				assert tokens.length == 7 + predicates.size();
+
+				for (int i = 7; i < tokens.length; i++) {
+					if (!tokens[i].equals(Constants.UNDEFINED)) {
+						graph.addEdge(predicates.get(i - 7), id, tokens[i]);
+					}
+				}
+				id++;
+			}
+
+			// If a node is labeled as a PRED, it should have outgoing edges.
+			for (Node node : graph.getNodes()) {
+				assert !node.isPred || node.hasOutgoingEdges();
+			}
+
+			return graph;
+		}
+	}
+}
---
+++ src/sdp/io/GraphWriter2015.java
@@ -0,0 +1,127 @@
+/*
+ * See the file "LICENSE" for the full license governing this code.
+ */
+package sdp.io;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import sdp.graph.Edge;
+import sdp.graph.Graph;
+import sdp.graph.Node;
+
+/**
+ * Write semantic dependency graphs to a file.
+ *
+ * @author Marco Kuhlmann <marco.kuhlmann@liu.se>
+ */
+public class GraphWriter2015 {
+
+    /**
+     * The low-level writer.
+     */
+    private final PrintWriter writer;
+
+    /**
+     * Create a graph writer that writes to the specified PrintWriter.
+     *
+     * @param writer the PrintWriter to be written to
+     */
+    public GraphWriter2015(PrintWriter writer) {
+        this.writer = writer;
+    }
+
+    /**
+     * Create a graph writer that writes to the specified file.
+     *
+     * @param file the file to write to
+     * @throws IOException if the specified file does not exist, is a directory
+     * rather than a regular file, or for some other reason cannot be opened for
+     * writing
+     */
+    public GraphWriter2015(File file) throws IOException {
+        this(new PrintWriter(new BufferedWriter(new FileWriter(file))));
+    }
+
+    /**
+     * Create a graph writer that writes to the specified file.
+     *
+     * @param fileName the name of the file to read from
+     * @throws IOException if the specified file does not exist, is a directory
+     * rather than a regular file, or for some other reason cannot be opened for
+     * writing
+     */
+    public GraphWriter2015(String fileName) throws IOException {
+        this(new File(fileName));
+    }
+
+    /**
+     * Writes a single graph.
+     *
+     * @param graph the graph to be written
+     * @throws IOException if an I/O error occurs
+     */
+    public void writeGraph(Graph graph) throws IOException {
+        int nNodes = graph.getNNodes();
+
+        String[][] labels = new String[nNodes][nNodes];
+        for (Edge edge : graph.getEdges()) {
+            labels[edge.source][edge.target] = edge.label;
+        }
+
+        writer.println(graph.id);
+
+        for (Node node : graph.getNodes()) {
+            if (node.id > 0) {
+                StringBuilder sb = new StringBuilder();
+                // Field 1: ID
+                sb.append(Integer.toString(node.id));
+                sb.append(Constants.COLUMN_SEPARATOR);
+                // Field 2: FORM
+                sb.append(node.form);
+                sb.append(Constants.COLUMN_SEPARATOR);
+                // Field 3: LEMMA
+                sb.append(node.lemma);
+                sb.append(Constants.COLUMN_SEPARATOR);
+                // Field 4: POS
+                sb.append(node.pos);
+                sb.append(Constants.COLUMN_SEPARATOR);
+                // Field 5: TOP
+                sb.append(node.isTop ? "+" : "-");
+                sb.append(Constants.COLUMN_SEPARATOR);
+                // Field 6: PRED
+                sb.append(node.isPred ? "+" : "-");
+                sb.append(Constants.COLUMN_SEPARATOR);
+                // Field 7: SENSE
+                sb.append(node.sense);
+
+                for (Node source : graph.getNodes().subList(1, nNodes)) {
+                    if (source.isPred) {
+                        sb.append(Constants.COLUMN_SEPARATOR);
+                        String label = labels[source.id][node.id];
+                        if (label == null) {
+                            sb.append(Constants.UNDEFINED);
+                        } else {
+                            sb.append(label);
+                        }
+                    }
+                }
+
+                writer.println(sb.toString());
+            }
+        }
+
+        writer.println();
+    }
+
+    /**
+     * Closes the stream and releases any system resources associated with it.
+     *
+     * @throws IOException if an I/O error occurs
+     */
+    public void close() throws IOException {
+        writer.close();
+    }
+}
