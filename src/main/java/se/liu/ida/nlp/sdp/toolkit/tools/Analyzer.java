1f2c76fac899fdcac7ef8df9d88efb3d09adf627
Marco Kuhlmann <marco.kuhlmann@liu.se>
2015-02-07 15:21:12 +0100

    Add sense statistics

--- src/main/java/se/liu/ida/nlp/sdp/toolkit/tools/Analyzer.java
+++ src/main/java/se/liu/ida/nlp/sdp/toolkit/tools/Analyzer.java
@@ -21,165 +21,181 @@ import se.liu.ida.nlp.sdp.toolkit.io.GraphReader2015;
  */
 public class Analyzer {
 
-	// The number of graphs read.
-	private int nGraphs;
+    // The number of graphs read.
+    private int nGraphs;
 
-	// The number of non-wall nodes seen.
-	private int nNonWallNodes;
+    // The number of non-wall nodes seen.
+    private int nNonWallNodes;
 
-	// The number of edges seen.
-	private int nEdges;
+    // The number of edges seen.
+    private int nEdges;
 
-	// Number of labels seen.
-	private final Set<String> labels = new HashSet<String>();
+    // Set of labels seen.
+    private final Set<String> labels = new HashSet<String>();
 
-	// The number of singleton nodes.
-	private int nSingletons;
+    // The number of singleton nodes.
+    private int nSingletons;
 
-	// Number of cyclic graphs.
-	private int nCyclic;
+    // Number of cyclic graphs.
+    private int nCyclic;
 
-	// Number of forests.
-	private int nForests;
+    // Number of forests.
+    private int nForests;
 
-	// Number of trees.
-	private int nTrees;
+    // Number of trees.
+    private int nTrees;
 
-	// Number of graphs that are semi-connected.
-	private int nFragmented;
+    // Number of graphs that are semi-connected.
+    private int nFragmented;
 
-	// Number of nodes that have more than one incoming edge.
-	private int nReentrantNodes;
+    // Number of nodes that have more than one incoming edge.
+    private int nReentrantNodes;
 
-	// Number of topless graphs.
-	private int nToplessGraphs;
+    // Number of topless graphs.
+    private int nToplessGraphs;
 
-	// Number of top nodes.
-	private int nTopNodes;
+    // Number of top nodes.
+    private int nTopNodes;
 
-	// Number of special nodes.
-	private int nSpecialNodes;
+    // Number of special nodes.
+    private int nSpecialNodes;
 
-	// Number of noncrossing graphs.
-	private int nNoncrossingGraphs;
+    // Number of noncrossing graphs.
+    private int nNoncrossingGraphs;
 
-	// Number of projective graphs.
-	private int nProjectiveGraphs;
+    // Number of projective graphs.
+    private int nProjectiveGraphs;
 
-	/**
-	 * Updates the statistics with the specified graph.
-	 *
-	 * @param graph a semantic dependency graph
-	 */
-	public void update(Graph graph) {
-		InspectedGraph inspectedGraph = new InspectedGraph(graph);
+    // Set of senses seen.
+    private final Set<String> senses = new HashSet<String>();
 
-		// number of graphs
-		nGraphs++;
+    // Number of (non-singleton) nodes with senses.
+    private int nNodesWithSenses;
 
-		// number of non-wall nodes
-		nNonWallNodes += inspectedGraph.getNNonWallNodes();
+    /**
+     * Updates the statistics with the specified graph.
+     *
+     * @param graph a semantic dependency graph
+     */
+    public void update(Graph graph) {
+        InspectedGraph inspectedGraph = new InspectedGraph(graph);
 
-		// number of edges
-		nEdges += graph.getNEdges();
+        // number of graphs
+        nGraphs++;
 
-		// distinct labels
-		for (Edge edge : graph.getEdges()) {
-			labels.add(edge.label);
-		}
+        // number of non-wall nodes
+        nNonWallNodes += inspectedGraph.getNNonWallNodes();
 
-		// number of singleton nodes
-		nSingletons += inspectedGraph.getNSingletons();
+        // number of edges
+        nEdges += graph.getNEdges();
 
-		// number of cyclic graphs
-		nCyclic += inspectedGraph.isCyclic() ? 1 : 0;
+        // distinct labels
+        for (Edge edge : graph.getEdges()) {
+            labels.add(edge.label);
+        }
 
-		// number of forests
-		nForests += inspectedGraph.isForest() ? 1 : 0;
+        // number of singleton nodes
+        nSingletons += inspectedGraph.getNSingletons();
 
-		// number of trees
-		nTrees += inspectedGraph.isTree() ? 1 : 0;
+        // number of cyclic graphs
+        nCyclic += inspectedGraph.isCyclic() ? 1 : 0;
 
-		// number of graphs that are fragmented
-		nFragmented += inspectedGraph.getNComponents() - 1 - inspectedGraph.getNSingletons() == 1 ? 0 : 1;
+        // number of forests
+        nForests += inspectedGraph.isForest() ? 1 : 0;
 
-		// number of reentrant nodes
-		for (Node node : graph.getNodes()) {
-			nReentrantNodes += node.getNIncomingEdges() > 1 ? 1 : 0;
-		}
+        // number of trees
+        nTrees += inspectedGraph.isTree() ? 1 : 0;
 
-		// number of topless graphs
-		boolean isTopless = true;
-		for (Node node : graph.getNodes()) {
-			isTopless = isTopless && !node.isTop;
-		}
-		nToplessGraphs += isTopless ? 1 : 0;
+        // number of graphs that are fragmented
+        nFragmented += inspectedGraph.getNComponents() - 1 - inspectedGraph.getNSingletons() == 1 ? 0 : 1;
 
-		// number of top nodes
-		for (Node node : graph.getNodes()) {
-			nTopNodes += node.isTop ? 1 : 0;
-		}
+        // number of reentrant nodes
+        for (Node node : graph.getNodes()) {
+            nReentrantNodes += node.getNIncomingEdges() > 1 ? 1 : 0;
+        }
 
-		// number of special nodes
-		for (Node node : graph.getNodes()) {
-			nSpecialNodes += node.id != 0 && !inspectedGraph.isSingleton(node.id) && !node.hasIncomingEdges() && !node.isTop ? 1 : 0;
-		}
+        // number of topless graphs
+        boolean isTopless = true;
+        for (Node node : graph.getNodes()) {
+            isTopless = isTopless && !node.isTop;
+        }
+        nToplessGraphs += isTopless ? 1 : 0;
 
-		// number of noncrossing graphs
-		nNoncrossingGraphs += inspectedGraph.isNoncrossing() ? 1 : 0;
+        // number of top nodes
+        for (Node node : graph.getNodes()) {
+            nTopNodes += node.isTop ? 1 : 0;
+        }
 
-		// number of projective graphs
-		nProjectiveGraphs += inspectedGraph.isProjective() ? 1 : 0;
-	}
+        // number of special nodes
+        for (Node node : graph.getNodes()) {
+            nSpecialNodes += node.id != 0 && !inspectedGraph.isSingleton(node.id) && !node.hasIncomingEdges() && !node.isTop ? 1 : 0;
+        }
 
-	/**
-	 * Prints statistics about a set of graphs.
-	 *
-	 * @param args names of files from which to read graphs
-	 * @throws Exception if an I/O exception occurs
-	 */
-	public static void main(String[] args) throws Exception {
-		Analyzer analyzer = new Analyzer();
-		for (String arg : args) {
-			GraphReader reader = new GraphReader2015(arg);
-			Graph graph;
-			while ((graph = reader.readGraph()) != null) {
-				analyzer.update(graph);
-			}
-			reader.close();
-		}
-		System.err.format("number of graphs: %d%n", analyzer.nGraphs);
-		System.err.format("number of non-wall nodes: %d%n", analyzer.nNonWallNodes);
-		System.err.format("number of distinct labels: %d%n", analyzer.labels.size());
-		System.err.format("percentage of singleton nodes (over all non-wall nodes): %s%n", percentage(analyzer.nSingletons, analyzer.nNonWallNodes));
-		System.err.format("edge density (proportion of edge counts to non-singleton nodes): %s%n", fraction(analyzer.nEdges, analyzer.nNonWallNodes - analyzer.nSingletons));
-		System.err.format("percentage of cyclic graphs: %s%n", percentage(analyzer.nCyclic, analyzer.nGraphs));
-		System.err.format("percentage of graphs that are forests: %s%n", percentage(analyzer.nForests, analyzer.nGraphs));
-		System.err.format("percentage of graphs that are trees: %s%n", percentage(analyzer.nTrees, analyzer.nGraphs));
-		System.err.format("percentage of graphs that are fragmented: %s%n", percentage(analyzer.nFragmented, analyzer.nGraphs));
-		System.err.format("percentage of (non-singleton) nodes that are reentrant: %s%n", percentage(analyzer.nReentrantNodes, analyzer.nNonWallNodes - analyzer.nSingletons));
-		System.err.format("percentage of topless graphs: %s%n", percentage(analyzer.nToplessGraphs, analyzer.nGraphs));
-		System.err.format("average number of top nodes per graph: %s%n", fraction(analyzer.nTopNodes, analyzer.nGraphs));
-		System.err.format("percentage of (non-singleton) nodes with indegree = 0 that are not top: %s%n", percentage(analyzer.nSpecialNodes, analyzer.nNonWallNodes - analyzer.nSingletons));
-		System.err.format("percentage of noncrossing graphs: %s%n", percentage(analyzer.nNoncrossingGraphs, analyzer.nGraphs));
-		System.err.format("percentage of projective graphs: %s%n", percentage(analyzer.nProjectiveGraphs, analyzer.nGraphs));
-	}
+        // number of noncrossing graphs
+        nNoncrossingGraphs += inspectedGraph.isNoncrossing() ? 1 : 0;
 
-	private static final NumberFormat NUMBER_FORMAT = NumberFormat.getIntegerInstance(Locale.ENGLISH);
-	private static final NumberFormat PERCENT_FORMAT = NumberFormat.getPercentInstance(Locale.ENGLISH);
+        // number of projective graphs
+        nProjectiveGraphs += inspectedGraph.isProjective() ? 1 : 0;
 
-	static {
-		NUMBER_FORMAT.setMinimumFractionDigits(4);
-		NUMBER_FORMAT.setMaximumFractionDigits(4);
-		PERCENT_FORMAT.setMinimumFractionDigits(2);
-		PERCENT_FORMAT.setMaximumFractionDigits(2);
-	}
+        // number of senses
+        for (Node node : graph.getNodes()) {
+            if (node.id != 0 && !inspectedGraph.isSingleton(node.id) && node.isPred && !node.sense.equals("_")) {
+                senses.add(node.sense);
+                nNodesWithSenses += 1;
+            }
+        }
+    }
 
-	public static String fraction(int a, int b) {
-		return NUMBER_FORMAT.format((double) a / (double) b);
-	}
+    /**
+     * Prints statistics about a set of graphs.
+     *
+     * @param args names of files from which to read graphs
+     * @throws Exception if an I/O exception occurs
+     */
+    public static void main(String[] args) throws Exception {
+        Analyzer analyzer = new Analyzer();
+        for (String arg : args) {
+            GraphReader reader = new GraphReader2015(arg);
+            Graph graph;
+            while ((graph = reader.readGraph()) != null) {
+                analyzer.update(graph);
+            }
+            reader.close();
+        }
+        System.err.format("number of graphs: %d%n", analyzer.nGraphs);
+        System.err.format("number of non-wall nodes: %d%n", analyzer.nNonWallNodes);
+        System.err.format("number of distinct labels: %d%n", analyzer.labels.size());
+        System.err.format("percentage of singleton nodes (over all non-wall nodes): %s%n", percentage(analyzer.nSingletons, analyzer.nNonWallNodes));
+        System.err.format("edge density (proportion of edge counts to non-singleton nodes): %s%n", fraction(analyzer.nEdges, analyzer.nNonWallNodes - analyzer.nSingletons));
+        System.err.format("percentage of cyclic graphs: %s%n", percentage(analyzer.nCyclic, analyzer.nGraphs));
+        System.err.format("percentage of graphs that are forests: %s%n", percentage(analyzer.nForests, analyzer.nGraphs));
+        System.err.format("percentage of graphs that are trees: %s%n", percentage(analyzer.nTrees, analyzer.nGraphs));
+        System.err.format("percentage of graphs that are fragmented: %s%n", percentage(analyzer.nFragmented, analyzer.nGraphs));
+        System.err.format("percentage of (non-singleton) nodes that are reentrant: %s%n", percentage(analyzer.nReentrantNodes, analyzer.nNonWallNodes - analyzer.nSingletons));
+        System.err.format("percentage of topless graphs: %s%n", percentage(analyzer.nToplessGraphs, analyzer.nGraphs));
+        System.err.format("average number of top nodes per graph: %s%n", fraction(analyzer.nTopNodes, analyzer.nGraphs));
+        System.err.format("percentage of (non-singleton) nodes with indegree = 0 that are not top: %s%n", percentage(analyzer.nSpecialNodes, analyzer.nNonWallNodes - analyzer.nSingletons));
+        System.err.format("percentage of noncrossing graphs: %s%n", percentage(analyzer.nNoncrossingGraphs, analyzer.nGraphs));
+        System.err.format("percentage of projective graphs: %s%n", percentage(analyzer.nProjectiveGraphs, analyzer.nGraphs));
+        System.err.format("number of distinct senses: %d%n", analyzer.senses.size());
+        System.err.format("percentage of (non-singleton) nodes with senses: %s%n", percentage(analyzer.nNodesWithSenses, analyzer.nNonWallNodes - analyzer.nSingletons));
+    }
 
-	public static String percentage(int enumerator, int denominator) {
-		return PERCENT_FORMAT.format((double) enumerator / (double) denominator);
-	}
+    private static final NumberFormat NUMBER_FORMAT = NumberFormat.getIntegerInstance(Locale.ENGLISH);
+    private static final NumberFormat PERCENT_FORMAT = NumberFormat.getPercentInstance(Locale.ENGLISH);
+
+    static {
+        NUMBER_FORMAT.setMinimumFractionDigits(4);
+        NUMBER_FORMAT.setMaximumFractionDigits(4);
+        PERCENT_FORMAT.setMinimumFractionDigits(2);
+        PERCENT_FORMAT.setMaximumFractionDigits(2);
+    }
+
+    public static String fraction(int a, int b) {
+        return NUMBER_FORMAT.format((double) a / (double) b);
+    }
+
+    public static String percentage(int enumerator, int denominator) {
+        return PERCENT_FORMAT.format((double) enumerator / (double) denominator);
+    }
 }
